60c5b7a25a34161199777ca22925de733db1f8d0
jordan <41rjordan@proton.me>
2024-10-19 15:26:29 +0500

    coderabbit suggestions

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
     return (
         <header className={styles.Header}>
             <img src="/images/logo.png" className={styles.Logo} alt="" />
-            <a href="https://ultrasoundlabs.github.io/untron-docs/" target="_blank" className={styles.DocsLink}>
+            <a href="https://ultrasoundlabs.github.io/untron-docs/" target="_blank" rel="noopener noreferrer" className={styles.DocsLink}>
                 Docs
             </a>
         </header>
