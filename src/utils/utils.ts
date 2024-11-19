46b91981b5ec83dd72b20481b2dc46eadce455d6
jordan <41rjordan@proton.me>
2024-11-19 17:09:45 +0700

    Revert "try not using the offset prefix"
    This reverts commit a6de7668917abd4b5b8fc9c782ad6bbd024b2ab1.

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -245,6 +245,10 @@ export function encodeIntent(intent: Intent): `0x${string}` {
         intent.outputAmount,
     ]);
 
-    const offsetEncodedIntent = encodedIntent;
+    const offsetEncodedIntent = (
+        '0x0000000000000000000000000000000000000000000000000000000000000020' +
+        encodedIntent.slice(2)
+    ) as `0x${string}`;
+
     return offsetEncodedIntent;
 }
