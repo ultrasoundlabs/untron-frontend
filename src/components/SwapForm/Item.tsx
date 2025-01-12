245a9693a2b37a2d98411eb10e7829cd6479d26b
jordan <41rjordan@proton.me>
2025-01-12 16:26:12 +0500

    move the dropdown indicator to the left

--- src/components/SwapForm/Item.tsx
+++ src/components/SwapForm/Item.tsx
@@ -17,12 +17,14 @@ const AssetDisplay = ({
       onClick={onClick}
       disabled={disableAssetSelection}
     >
+      {!disableAssetSelection && (
+        <span className={styles.DropdownIndicator}>
+          <svg width="12" height="8" viewBox="0 0 12 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M1 1.5L6 6.5L11 1.5" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+          </svg>
+        </span>
+      )}
       <img src={icon} alt="Token icon" />
-      <span className={styles.DropdownIndicator}>
-        <svg width="12" height="8" viewBox="0 0 12 8" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M1 1.5L6 6.5L11 1.5" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-        </svg>
-      </span>
     </button>
   );
 };
