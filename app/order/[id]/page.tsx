4c6b6e30dc77afdeea5ba2e85e8c2786493c419f
jordan <41rjordan@proton.me>
2025-05-06 01:37:11 +0500

    prepare for production build

--- app/order/[id]/page.tsx
+++ app/order/[id]/page.tsx
@@ -2,14 +2,8 @@
 
 import UntronInterface from "@/components/untron/untron-interface"
 
-interface OrderPageProps {
-  params: {
-    id: string
-  }
-}
-
-export default function OrderPage({ params }: OrderPageProps) {
+export default function OrderPage() {
   // The id can be used later when real logic is implemented.
   // For now we just render the swap interface.
   return <UntronInterface />
-} 
+}
