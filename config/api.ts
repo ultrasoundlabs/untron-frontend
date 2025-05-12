d3b4775256573c041aa30babbdb0b7c6f2a51c81
jordan <41rjordan@proton.me>
2025-05-12 18:25:18 +0500

    chore: update the recommended swap rate

--- config/api.ts
+++ config/api.ts
@@ -6,6 +6,6 @@ export interface ApiInfoResponse {
   retryAtS: number
 }
 
-// Swap rate represented in smallest units (6 decimals) to avoid floats: 0.997 -> 997000 / 1_000_000
+// Swap rate represented in smallest units (6 decimals) to avoid floats: 0.9997 -> 999700 / 1_000_000
 // Swap rate is hardcoded for now but can be easily changed to use API in the future
-export const SWAP_RATE_UNITS: bigint = 997000n;
+export const SWAP_RATE_UNITS: bigint = 999700n;
